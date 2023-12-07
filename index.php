80c77c243a7fff5bcc0f4b8132fbb70d9b6d7bc3
Cameron Cardinal <cameriancardinal@gmail.com>
2023-12-07 10:15:17 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- Exercice 1 
+Ecrire un algorithme permettant de saisir 5 réelles au clavier, les stocker dans un tableau, calculer leur somme et les afficher avec leur somme à l’écran.  -->
+
+<?php
+$nombre1 = intval(readline("Entrez une première réelle :"));
+$nombre2 = intval(readline("Entrez une deuxième réelle :"));
+$nombre3 = intval(readline("Entrez une troisième réelle :"));
+$nombre4 = intval(readline("Entrez une quatrième réelle :"));
+$nombre5 = intval(readline("Entrez une cinquième réelle :"));
+
+$nombres = array($nombre1, $nombre2, $nombre3, $nombre4, $nombre5);
+
+$somme = $nombre1 + $nombre2 + $nombre3 + $nombre4 + $nombre5;
+
+print_r($nombres);
+echo $somme;
+?>
